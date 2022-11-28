346242b442216533cee5d08b9ee2f7ca33ba5860
tinrafiq <mehmetozturan@proton.me>
2022-11-28 21:24:31 +0300

    Added some comments to make it clear

--- mqtt/subscribe.py
+++ mqtt/subscribe.py
@@ -10,22 +10,23 @@ client_id = ''
 username = 'tinrafiq'
 password = "j8ktX@W7'Qw"
 
-
 def connect_mqtt():
 
+    #* RC returns either zero or one due to connection status
     def on_connect(client, userdata, flags, rc):
         if rc==0:
             print("Successfully connected to MQTT broker")
         else:
             print("Failed to connect, return code %d", rc)
  
- 
+    #* set as a client (node)
     client = mqtt_client.Client(client_id)
 
     #* enable TLS for secure connection
+    #* some mqtt broker services use this
     client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
 
-
+    #* Broker login infos
     client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
@@ -33,9 +34,11 @@ def connect_mqtt():
 
 
 def subscribe(client: mqtt_client):
+
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
 
+    #* subscribe to a certain topic and print it
     client.subscribe(topic)
     client.on_message = on_message
 
